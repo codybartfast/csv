a9876725b684843e88fa5a5994cefeaa7660fa9d
John Donnellan <john@codybartfast.com>
2022-06-28 09:29:22 +0100

    Add Cell parsing tests

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
+# CSV
+
 Csv seemed like a good idea at 2022-06-23T08:54:30.3216162Z.
 
 Not for editing or two way mod
 pass a function for di
 Custom converters, percent, culture!
+scientific
--- tests/Csv.Tests/CellTests.cs
+++ tests/Csv.Tests/CellTests.cs
@@ -35,27 +35,100 @@ public class CellTests
 
 
     [Fact]
-    public void NastyString(){
+    public void NastyString()
+    {
         // more compresenisve tests in CsvParserTests
         var text = "\r \" \",\" ,, \"\"\r\"";
         Assert.Equal(text, Cell.From(text));
     }
 
     [Fact]
-    public void SortableDateTime(){
+    public void SortableDateTime()
+    {
         var date = DateTime.Parse("2002-03-04T05:06Z");
         Assert.Equal(date, Cell.From("2002-03-04 5:06"));
     }
 
     [Fact]
-    public void SortableDate(){
+    public void SortableDate()
+    {
         var date = DateTime.Parse("2002-03-04");
         Assert.Equal(date, Cell.From("2002-03-04"));
     }
 
     [Fact]
-    public void StupidAmericanStyleDate(){
+    public void StupidAmericanStyleDate()
+    {
         var date = DateTime.Parse("2002-03-04");
         Assert.Equal(date, Cell.From("03/04/02"));
-    }    
+    }
+
+    [Fact]
+    public void Integers()
+    {
+        int i = 1234;
+        Assert.Equal(i, Cell.From("1234"));
+        Assert.Equal(i, Cell.From("001234"));
+        Assert.Equal(i, Cell.From("1,234"));
+        Assert.Equal(i, Cell.From(" 1,234 "));
+        Assert.Equal(i, Cell.From(" 1234 "));
+        Assert.Equal(-i, Cell.From(" -1,234 "));
+
+        uint u = 1234u;
+        Assert.Equal(u, Cell.From("1234"));
+        Assert.Equal(u, Cell.From("001234"));
+        Assert.Equal(u, Cell.From("1,234"));
+        Assert.Equal(u, Cell.From(" 1,234 "));
+        Assert.Equal(u, Cell.From(" 1234 "));
+        Action uNeg = () => { var _ = (uint)Cell.From(" -1,234 "); };
+        Assert.Throws<OverflowException>(uNeg);
+
+        long l = 1234;
+        Assert.Equal(l, Cell.From("1234"));
+        Assert.Equal(l, Cell.From("001234"));
+        Assert.Equal(l, Cell.From("1,234"));
+        Assert.Equal(l, Cell.From(" 1,234 "));
+        Assert.Equal(l, Cell.From(" 1234 "));
+        Assert.Equal(-l, Cell.From(" -1,234 "));
+
+        ulong ul = 1234;
+        Assert.Equal(ul, Cell.From("1234"));
+        Assert.Equal(ul, Cell.From("001234"));
+        Assert.Equal(ul, Cell.From("1,234"));
+        Assert.Equal(ul, Cell.From(" 1,234 "));
+        Assert.Equal(ul, Cell.From(" 1234 "));
+        Action ulNeg = () => { var _ = (ulong)Cell.From(" -1,234 "); };
+        Assert.Throws<OverflowException>(ulNeg);
+    }
+
+    [Fact]
+    public void NonInteger()
+    {
+        float f = 1234.567f;
+        Assert.Equal(f, Cell.From("1234.567"));
+        Assert.Equal(f, Cell.From("001234.56700"));
+        Assert.Equal(f, Cell.From("1,234.567"));
+        Assert.Equal(f, Cell.From(" 1,234.567 "));
+        Assert.Equal(f, Cell.From(" 1234.567 "));
+        Assert.Equal(-f, Cell.From(" -1,234.567 "));
+        Assert.Equal(-f, Cell.From(" -01.2345670E+3 "));
+
+        double d = 1234.567;
+        Assert.Equal(d, Cell.From("1234.567"));
+        Assert.Equal(d, Cell.From("001234.56700"));
+        Assert.Equal(d, Cell.From("1,234.567"));
+        Assert.Equal(d, Cell.From(" 1,234.567 "));
+        Assert.Equal(d, Cell.From(" 1234.567 "));
+        Assert.Equal(-d, Cell.From(" -1,234.567 "));
+        Assert.Equal(-d, Cell.From(" -01.2345670E+3 "));
+
+        decimal m = 1234.567m;
+        Assert.Equal(m, Cell.From("1234.567"));
+        Assert.Equal(m, Cell.From("001234.56700"));
+        Assert.Equal(m, Cell.From("1,234.567"));
+        Assert.Equal(m, Cell.From(" 1,234.567 "));
+        Assert.Equal(m, Cell.From(" 1234.567 "));
+        Assert.Equal(-m, Cell.From(" -1,234.567 "));
+        Assert.Equal(-m, Cell.From(" -01.2345670E+3 "));
+    }
 }
